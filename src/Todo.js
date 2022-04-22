ba1f5b068346c5a15f25e70e2ffe1c9855a908b6
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-22 09:47:48 +0100

    final change

--- src/Todo.js
+++ src/Todo.js
@@ -1,72 +1,64 @@
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 
 const Todo = () => {
-    const [tasks, setTasks] = useState([])
-    const [input, setInput] = useState("")
-    const [done, setDone] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
 
-    const updateInput = (event) => {
-        setInput(event.target.value)
-    } 
-    const addTask = () => {
-        let array = [...tasks]
-        array.push(input)
-        setTasks(array)
-        setInput("")
-    }
-    const removeTasks = (index) => {
-        let array = [...tasks];
-        array.splice(index, 1);
-        setTasks(array)
-    }
-    // const checkTasks = (index) => {
-    //     let array = [...tasks];
-    //     array(index)               
-    //     setDone(array)
-    // }
+  const updateInput = (event) => {
+    setInput(event.target.value);
+  };
 
-    return (
-        <div>
-            <h1>To-Do list</h1>
-            <input className="addTask1" value={input} onChange={updateInput}/>
-            <button className="addTask" onClick={addTask}>Add Task</button>
+  //amended push method with new properties that will recognise state change
+  const addTask = () => {
+    let array = [...tasks];
+    array.push({ text: input, complete: false });
+    setTasks(array);
+    setInput("");
+  };
+  const removeTasks = (index) => {
+    let array = [...tasks];
+    array.splice(index, 1);
+    setTasks(array);
+  };
 
+  //funtion to set the state to complete line through, green
+  const checkBox = (index) => {
+    const newTasks = tasks.slice();
+    newTasks[index].complete = !newTasks[index].complete;
+    setTasks(newTasks);
+  };
 
+  return (
+    <div>
+      <h1>To-Do list</h1>
+      <input className="addTask1" value={input} onChange={updateInput} />
+      <button className="addTask" onClick={addTask}>
+        Add Task
+      </button>
 
-
-            {tasks.map((item, index) => {
-                return (
-                <div className="container">
-                    {done ? (
-                        <h1 style={{color: "green", textDecoration: "line-through"}}>
-                            {item}
-                        </h1>
-                    ) : (
-                        <h1>{item}</h1>
-                        )}
-                        <button className="check" onClick={() => setDone(true)}>✔</button>
-                        <button className="delete" onClick={() => removeTasks(index)}>X</button>
-                    </div>)
-                {/* <button className="check" onClick={() => checkTasks(index)}
-                style={{
-                done:color ? 'red':'green'
-            // done:border ? 'red':'green'
-            }}
-
-                    >✔</button>  */}
-                {/* <button className="check" onClick={() => setDone(!done)}
-                style={
-                    {
-                    done:color ? 'red':'green'
-                    // done:border ? 'red':'green'
-                    }
-                    }
-                >✔</button> */}
-                 {/* <h2>{item}</h2> */}
-            }
+      {tasks.map((item, index) => {
+        return (
+          <div className="container">
+            {item.complete ? (
+              <h1 style={{ color: "green", textDecoration: "line-through" }}>
+                {item.text}
+              </h1>
+            ) : (
+              <h1>{item.text}</h1>
             )}
-            </div>   
-            )}
-            
-export default Todo
+            {/* added function to onClick event */}
+            <button className="check" onClick={() => checkBox(index)}>
+              ✔
+            </button>
+            <button className="delete" onClick={() => removeTasks(index)}>
+              X
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Todo;
